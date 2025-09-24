ed5d043820f6b51a15a8af2a66b64d54f0f6f139
Jules Leguy; Chaïmaâ TOUHAMI; Gaëlle Milon-Harnois <gaelle.milon-harnois@univ-angers.fr>
2025-09-24 15:40:30 +0200

    scripts commented version

--- scripts/1.execution_script+sauvegarde_sortie_th0.py
+++ scripts/1.execution_script+sauvegarde_sortie_th0.py
@@ -12,9 +12,9 @@ date = str(datetime.date.today())
 path = "scripts/"
 file = "Random + qlearning_script_n_parallel_run_several_config_th0"
 file_EvoMol ="Silly_random_script"
-#file_EvoMolRL = "qlearning_script_n_parallel_run_several_config_th0" 
+file_EvoMolRL = "qlearning_script_n_parallel_run_several_config_th0" 
 
 # script execution. Log saved in a txt file
 with open(f'output_{file}_{date}.txt', 'w') as f:
    subprocess.run([sys.executable, f'{path}{file_EvoMol}.py'], stdout=f, stderr=f)
-   #subprocess.run([sys.executable, f'{path}{file_EvoMolRL}.py'], stdout=f, stderr=f)
+   subprocess.run([sys.executable, f'{path}{file_EvoMolRL}.py'], stdout=f, stderr=f)
